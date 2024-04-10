0a07f74dd2d898202674981b74e2a1b4a6db33ea
Evgenii Anchugov <bergy@yandex.ru>
2024-04-10 20:21:25 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+//        String[] parts = reader.readLine().split(" ");
+//        int[] nums = new int[parts.length];
+//        for (int i = 0; i < parts.length; i++) {
+//            nums[i] = Integer.parseInt(parts[i]);
+//        }
+//        Solution.solution(nums);
+//        reader.close();
+//        writer.close();
+        QuickSort.solution(new int[]{3,14,1,7,9,8,11,6,4,2});
+    }
+}
